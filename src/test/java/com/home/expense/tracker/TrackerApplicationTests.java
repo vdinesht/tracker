a817b667d3d13adc6b259f94b2aef3f437ddd6e8
Dinesh Vincent <Vincent_Dinesh_T@cat.com>
2024-04-23 22:17:18 +0530

    Initial commit

---
+++ src/test/java/com/home/expense/tracker/TrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.home.expense.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
