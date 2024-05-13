459081e8a23b845c12c203eee49afd64037a0ff9
Dinesh Vincent <Vincent_Dinesh_T@cat.com>
2024-05-13 23:45:23 +0530

    Tokenizing ICICI Bank statement in progress.

--- src/test/java/com/home/expense/tracker/imports/ICICI/StatementCSVReaderTest.java
+++ src/test/java/com/home/expense/tracker/imports/ICICI/StatementCSVReaderTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -11,9 +13,24 @@ public class StatementCSVReaderTest {
 
     @Autowired
     StatementReader statementReader;
+
+    private int count = 0;
     @Test
     void testCSVReader(){
-        statementReader.getAllRows().forEach(System.out::println);
+        statementReader.getAllRows().forEach(e-> System.out.println(parseTransactionRemarks(e.transactionRemarks())));
         assertTrue(statementReader.getAllRows().size()>0);
+
+        System.out.println("Identified count: " + count);
+    }
+
+
+    private String parseTransactionRemarks(String raw){
+        String[] iciciTokens = {"UPI/", "ACH/" , "MMT/", "BIL/", "NEFT", "CMS/", "DMC/", "ATM/", "Int.Pd", "VSI/", "GIB/", "DCardfee", "VIN/", "NFS/", "DTK/", "CLG/"};
+
+        if (Arrays.stream(iciciTokens).anyMatch(raw::contains)) {
+            ++count;
+            return "";
+        }
+        else  return raw;
     }
 }
